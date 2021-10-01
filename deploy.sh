0da8b2f4c7ff06098e1538b6c35895c95a2b2a95
ernestognw <ernestognw@gmail.com>
2021-10-01 10:09:41 -0500

    class(11): IPFS deployment

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ./build
+FILES=$(find * -type f | awk -v q="'" '{print " -F " q "file=@\"" $0 "\";filename=\"" $0 "\"" q}')
+curl "https://ipfs.infura.io:5001/api/v0/add?pin=true&recursive=true&wrap-with-directory=true&cid-version=1" -vv -X POST $FILES
+cd ..
